d6c938fa1891eaff4f00466c66f4b5f27b739713
Anshuman Srivastava <73851357+ravenclaw-10@users.noreply.github.com>
2021-11-20 02:34:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ function activateCars(){
     cars = document.querySelectorAll('div.cars');
     for(var i=0; i < cars.length; i++){
         altitude = cars[i].dataset.altitude;
+        console.log(altitude);
         speed = cars[i].dataset.speed;
+        console.log(speed);
         distance = cars[i].dataset.carDistance;
+        console.log(distance);
         cars[i].style.top = altitude+"px";
         cars[i].style.transitionDuration = speed+"s";
         cars[i].style.left = distance+"px";
